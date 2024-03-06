20f306ff9a69ea461cbf493f70154402e7fd84be
Cristopher <cristophernamchee12@gmail.com>
2024-03-06 20:19:24 +0700

    feat: solve 142

---
+++ src/behavioral/mediator.rs
@@ -0,0 +1,10 @@
+// Mediator is a design pattern that simplifies
+// communication between dependencies. Instead of directly
+// communicating with each other, dependencies communicate through
+// intermediate object that forwards message to correct channel.
+//
+//
+
+pub struct Chatroom {
+    
+}
