c8a435eaa8da41bfb3f20a4493ff9baa53ffefb8
Cristopher <cristophernamchee12@gmail.com>
2024-03-07 21:36:43 +0700

    feat: explain observer

---
+++ src/behavioral/observer.rs
@@ -0,0 +1,5 @@
+// Observer is a design pattern that allows you to create subscription
+// system that notify observants on certain events
+//
+// The most famous way to implement mediator is through observer pattern,
+// so I won't put any example here until the mediator pattern example changed.
